d12c51ad19348e9dcdd139e7ce239c03adea1f3d
Daniel Lima <dnlima77237@gmail.com>
2024-11-06 17:04:59 -0300

    changed to english

--- montyhall/main.py
+++ montyhall/main.py
@@ -6,6 +6,6 @@ AMOSTRA = 500_000
 
 with open("result.csv", "wt") as f:
     wt = csv.writer(f)
-    wt.writerow(["Sem trocar", "Trocando"])
+    wt.writerow(["not_changing", "changing"])
     for _ in range(AMOSTRA):
         wt.writerow([monty_hall(False), monty_hall(True)])
