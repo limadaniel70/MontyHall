f7926f9e3bc9a6cf8899af7c8a9144aa02aed142
Daniel Lima <dnlima77237@gmail.com>
2024-12-24 17:15:18 -0300

    refac: simplified code

--- montyhall/data_analysis.py
+++ montyhall/data_analysis.py
@@ -2,10 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb
 
-
-def plot(data: list[tuple[int, int, int]], filename: str) -> None:
-    df = pd.DataFrame(data, columns=["id", "win_changing", "win_not_changing"]).drop(
-        columns=["id"]
-    )
-    sb.barplot(df)
-    plt.savefig(filename)
+df = pd.read_csv("montyhall.csv")
+sb.barplot(df)
+plt.savefig("montyhall.png")
